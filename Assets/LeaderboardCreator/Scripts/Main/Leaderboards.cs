6373392752e14f3d8d50aa89bf4082279ea16974
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-11-05 14:44:51 +0100

    made a leaderboard system

---
+++ Assets/LeaderboardCreator/Scripts/Main/Leaderboards.cs
@@ -0,0 +1,8 @@
+namespace Dan.Main
+{
+    public static class Leaderboards
+    {
+        public static LeaderboardReference DemoSceneLeaderboard = new LeaderboardReference("980324f10e2f03fc0fd6a38a70f88123919a6aed35a4aea86211ad58b68db9c4");
+        public static LeaderboardReference PowerPlantPanic = new LeaderboardReference("d924292bdc30ab31f0bf7f831714a995ab70eb720e4af9b36d33b0a9996b266d");
+    }
+}
