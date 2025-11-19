d88c66c9006b0463a77be2c528a675156155feac
TanguyBouckaert <tanguybouckaert.tb@gmail.com>
2025-11-19 16:53:37 +0100

    Working Random progressbar

--- Assets/Scripts/UI/ProgressBarChanging.cs
+++ Assets/Scripts/UI/ProgressBarChanging.cs
@@ -8,23 +8,43 @@ public class ProgressBarChanging : MonoBehaviour
     [SerializeField]
     private Image[] ProgressBars;
 
-    private float _newBarValue, _oldBarValue;
+    [SerializeField]
+    private float ThreshHold;
+
+    private float _newBarValue, _oldBarValue, _timerChangeValue = 0f;
+
+    private Coroutine _fillingBar;
 
     private void Start()
     {
-        _oldBarValue = 0f;
+        _oldBarValue = ProgressBars[0].fillAmount;
     }
 
     void Update()
     {
-        StartCoroutine(MoveProgressBar());
+        _timerChangeValue += Time.deltaTime;
+
+        if (_timerChangeValue >= ThreshHold)
+        {
+            if (_fillingBar != null)
+            {
+                StopCoroutine(_fillingBar);
+                _fillingBar = null;
+            }
+            else
+            {
+                _fillingBar = StartCoroutine(MoveProgressBar());
+                _timerChangeValue = 0f;
+            }    
+        }  
     }
 
     private IEnumerator MoveProgressBar()
     {
-        _newBarValue = Random.Range(0, 1);
+        _newBarValue = Random.Range(1, 101);
+        _newBarValue = _newBarValue / 100;
 
-        float duration = 2f;
+        float duration = 4f;
         float elapsed = 0f;
 
         while (elapsed < duration)
