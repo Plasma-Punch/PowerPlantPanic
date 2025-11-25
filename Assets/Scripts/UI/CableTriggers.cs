da18126013be63f4318eb225f93ac405662e9eee
PauwelsQuinten <pauwels.quinten2.0@gmail.com>
2025-11-25 15:52:19 +0100

    fixed a small problem with the fuse box

--- Assets/Scripts/UI/CableTriggers.cs
+++ Assets/Scripts/UI/CableTriggers.cs
@@ -7,6 +7,14 @@ public class CableTriggers : MonoBehaviour
     private GameEvent _placedCorrectly;
     private bool _isFilled;
     private Collider2D _collided;
+
+    private bool _hasTriggered;
+
+    private void OnEnable()
+    {
+        _isFilled = false;
+        _hasTriggered = false;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag != this.tag) return;
@@ -17,8 +25,10 @@ public class CableTriggers : MonoBehaviour
     public void CheckCorrectness(Component sender, object obj)
     {
         if (!_isFilled) return;
+        if (_hasTriggered) return;
         _collided.transform.parent = this.transform;
         _collided.transform.localPosition = Vector3.zero;
         _placedCorrectly.Raise(this, EventArgs.Empty);
+        _hasTriggered = true;
     }
 }
