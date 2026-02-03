fe2fbfa575b0ff68c7d4d0b92e2e92c7b8051d58
Pauwels Quinten <144774819+PauwelsQuinten@users.noreply.github.com>
2026-02-03 16:30:31 +0100

    fixed the lights and dialogue

--- Assets/Scripts/Managers/CentralLightManager.cs
+++ Assets/Scripts/Managers/CentralLightManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CentralLightManager : MonoBehaviour
 {
-    [SerializeField] private List<SpriteRenderer> _lights = new List<SpriteRenderer>();
-    [SerializeField] private GameEvent _Completed;
+    [SerializeField] 
+    private List<SpriteRenderer> _lights = new List<SpriteRenderer>();
+    [SerializeField]
+    private Sprite _litLight;
+    [SerializeField] 
+    private GameEvent _Completed;
 
     private int _lightIndex;
 
     public void TurnOnLight()
     {
-        if(_lightIndex <= 3)_lights[_lightIndex].color = Color.green;
+        if(_lightIndex <= 3)_lights[_lightIndex].sprite = _litLight;
         _lightIndex++;
         if (_lightIndex >= 4) _Completed.Raise(this, EventArgs.Empty);
     }
